2defa2e28f44a1737121c1c47ccd00bf81eb7c40
mehrad <mmfirst@protonmail.com>
2023-08-18 22:40:51 +0330

    modified:   functions/bot/bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,8 +1,10 @@
-const fs = require("fs").promises;
+// const fs = require("fs").promises;
 const { Telegraf } = require("telegraf");
 const { Position } = require("kokopu");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+let activeGame = false;
 
 bot.command("start", (ctx) => {
     console.log(ctx.from);
@@ -12,8 +14,85 @@ bot.command("start", (ctx) => {
         {}
     );
 });
-bot.command("hi", (ctx) => {
-    ctx.reply("HI");
+
+bot.command("newGame", async (ctx) => {
+    if (activeGame) {
+        cyx.reply("there is already an active game");
+        return;
+    }
+
+    let gameFile = [];
+    let pos = new Position();
+    let players = { w: ctx.from.username, b: "" };
+    let count = 1;
+
+    async function board(ctx) {
+        await ctx.reply(pos.ascii());
+    }
+
+    async function join(ctx) {
+        if (activeGame) {
+            ctx.reply("the game is already started");
+            return;
+        }
+
+        players.black = ctx.from.username;
+        activeGame = true;
+        await ctx.reply("Game Started");
+    }
+
+    async function play(ctx) {
+        if (!activeGame) {
+            ctx.reply("NO Active Game!");
+            return;
+        }
+
+        const currentUser = await ctx.from.username;
+
+        if (currentUser !== players.w && currentUser !== players.b) {
+            ctx.reply("You're not playing!");
+            return;
+        }
+
+        // if(players.pos.turn() !== currentUser)
+        let texts = ctx.message.text.split(" ");
+        let move = texts[1];
+
+        if (pos.play(move)) {
+            if (pos.turn() == "b") {
+                gameFile.push(`${count}. ${move}`);
+                // await fs.appendFile(gameFile, `${count}. ${move} `);
+            } else if (currentUser) {
+                // await fs.appendFile(gameFile, `${move} `);
+                gameFile.push(`${move} `);
+                count++;
+            }
+
+            await ctx.reply(`move played ${move}`);
+        } else {
+            await ctx.reply("Invalid move");
+        }
+    }
+
+    // await fs.writeFile(gameFile, `[Site chsss telegram bot]\n`);
+    // await fs.appendFile(gameFile, `[Date ${new Date().toISOString()}]\n`);
+    // await fs.appendFile(gameFile, `[White ${ctx.from.first_name}]\n`);
+    // await fs.appendFile(gameFile, `[Black ${ctx.from.first_name}]\n`);
+
+    await ctx.reply("waiting for black, join with /join");
+
+    bot.command("endGame", async (ctx) => {
+        activeGame = false;
+        // const pgn = await fs.readFile(gameFile, "utf-8");
+        await ctx.reply(gameFile);
+        // ctx.reply("game ended");
+    });
+
+    bot.command("board", board);
+    bot.command("join", join);
+    bot.command("play", play);
+
+    // im using telegraf js for creating a telegram bot i want to stop the function called on command newGame how can i do that
 });
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
